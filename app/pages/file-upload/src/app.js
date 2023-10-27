9983f1ca43193106b525206dae56493f010d1826
Arthur Saito <arthur_saito1998@hotmail.com>
2023-10-26 21:26:02 -0300

    fix: fixing debugger

--- app/pages/file-upload/src/app.js
+++ app/pages/file-upload/src/app.js
@@ -19,8 +19,11 @@ view.configureOnFileChange((file) => {
 
 async function fakeFetch() {
   const filePath = '/videos/frag_bunny.mp4';
-  const response = await fetch(filePath);
-  debugger;
+  const response = await fetch(filePath, { method: 'HEAD' });
+  response.headers.get('content-length');
+  const file = new File([await response.blob()], filePath, { type: 'video/mp4', lastModified: Date.now() });
+  const event = new Event('change');
+  Reflect.defineProperty(event, 'target', { value: { files: [file] } });
 }
 
 fakeFetch();
